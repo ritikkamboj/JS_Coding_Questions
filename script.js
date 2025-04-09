bfda6f21e5376e1d278d66b21a8fc7585ae08996
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-09 14:08:08 +0530

    three ways to revese the array

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-// Below are the methods which i like to solve the question of removing the duplocate elements 
+// Ways to solve the problem on how to reverse the array 
 
-const x = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+const x = [1, 2, 3, 4, 4, 5];
 
-const x1 = [...new Set(x)];
-console.log(x1);
+const x1 = x.reverse();
+console.log(x1)
 
-// second method  by using filter and index macyching 
+// above one also change the original array
 
-const x2 = x.filter((item, index) => x.indexOf(item) === index);
-console.log(x2);
+// Now for loop array way 
+const x3 = [1, 2, 3, 4, 5]
+const x2 = []
+for (let i = x3.length - 1; i >= 0; i--) {
+    x2.push(x3[i]);
+}
+console.log("reverse the array ", x2);
 
+// using temp variable way 
 
-//  using reduce method to remove the duplicate array 
+for (let i = 0; i < x3.length / 2; i++) {
+    let temp = x3[i];
+    x3[i] = x3[x3.length - 1 - i];
+    x3[x3.length - 1 - i] = temp;
+}
 
-const x3 = x.reduce((acc, cur) => {
-    if (!acc.includes(cur))
-        acc.push(cur)
-
-    return acc;
-
-}, [])
 console.log(x3);
