5f7062a4f1a5808670b797b7ecd78e69f4b9c3bf
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-10 18:18:29 +0530

    solutions to check the frequency of the numbers inside the array

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-// Array to check that if its palindrome or not 
+// we have to count the occurence of each element , we can solve this with the help of object approach 
+let freq = {};
+function frequency(arr) {
 
-// one is minimal approach , you can say leyman approach by using while loop 
 
-function checkPalindrome(arr) {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left < right) {
-        if (arr[left] !== arr[right]) return false
-        left++;
-        right--;
+    for (let item of arr) {
+        freq[item] = (freq[item] || 0) + 1
     }
-    return true
 }
 
-console.log(checkPalindrome([1, 2, 3, 2, 1]));
+frequency([1, 2, 3, 2, 1]);
+console.log(freq)
 
-// second approach is kind of modern approach 
 
-function checkPalindrome1(arr) {
-    let reverse = [...arr].reverse();
-    console.log(reverse);
-    // have to change both arr in string so that we can compair them
-    return arr.join() === reverse.join();
+// using forEach loop 
+
+function frequency1(arr) {
+    let freq = {};
+    arr.forEach((item) => {
+        freq[item] = (freq[item] || 0) + 1
+    })
+    return freq
 }
 
-console.log(checkPalindrome1([1, 2, 3, 2, 1]));
+console.log(frequency1([1, 2, 2, 3, 1]))
+
+// 3rd way is to use the reducer method 
+
+let arrd = [1, 2, 3, 2, 1, 3];
+
+let freq1 = arrd.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc
+}, {})
+console.log(freq1);
+
+
