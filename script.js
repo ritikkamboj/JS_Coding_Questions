3e3792b2603ace827645a0f5ae2b331769f99a06
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-10 14:57:49 +0530

    rotating array k times  3 easy ways

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// So we have to create the object which makes the data of frequency 
+// Writing a function  to rotate the array k times 
 
-const arr = [1, 1, 2, 3, 3, 5, 5, 5, 8, 8, 8, 8];
 
-const freq = {};
+function rotate(arr, k) {
+    k = k % arr.length;
 
-arr.find((item) => {
-    freq[item] = (freq[item] || 0) + 1;
-})
+    return [...arr.slice(-k), ...arr.slice(0, -k)]
+}
 
-// we cam use find and filter method here , 
-
-const array = Object.keys(freq);
-console.log(array)
-const x = array.find((item) => freq[item] === 1);
+const x = rotate([1, 2, 3, 4, 5], 2);
 console.log(x);
 
-// and if we want to find all the elements which comes only one times in the array 
-const x1 = array.filter((item) => freq[item] === 1);
-console.log(...x1);
+// we have two more methods to solve the rotating the array 
+// its by using splice and unshifts 
+
+
+function rotate1(arr, k) {
+    k = k % arr.length
+    let removed = arr.splice(-k);
+    console.log(removed)
+    arr.unshift(...removed);
+    return arr;
+}
+
+const newArr1 = rotate1([1, 2, 3, 4, 5], 2);
+console.log(newArr1)
+
+// 2 , its by using pop() and unshift() method 
+
+function rotate3(arr, k) {
+    k = k % arr.length
+
+    for (let i = 0; i < k; i++) {
+        arr.unshift(arr.pop());
+    }
+    return arr;
+}
+
+const newArr2 = rotate3([1, 2, 3, 4, 5], 2)
+console.log(newArr2)
