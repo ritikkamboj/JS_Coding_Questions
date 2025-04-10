5a9a73020ba63d077ddd5c7e65df421c03925dc1
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-10 19:21:50 +0530

    intersection of two arrays

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-// we have to count the occurence of each element , we can solve this with the help of object approach 
-let freq = {};
-function frequency(arr) {
+// we have to find the intersection of two arrays
+
+const x1 = [1, 2, 3, 1];
+const x2 = [2, 3, 4, 1];
+
+const intersect = x1.filter((item) => x2.includes(item));
+console.log(intersect)
+
+// second solution is to use th set concept , because it makes resuls faster
+
+const setx2 = new Set(x2);
+const intersect2 = x1.filter((item) => setx2.has(item));
+console.log(intersect2)
 
 
-    for (let item of arr) {
-        freq[item] = (freq[item] || 0) + 1
-    }
-}
+// 3rd silution is one liner solution 
 
-frequency([1, 2, 3, 2, 1]);
-console.log(freq)
-
-
-// using forEach loop 
-
-function frequency1(arr) {
-    let freq = {};
-    arr.forEach((item) => {
-        freq[item] = (freq[item] || 0) + 1
-    })
-    return freq
-}
-
-console.log(frequency1([1, 2, 2, 3, 1]))
-
-// 3rd way is to use the reducer method 
-
-let arrd = [1, 2, 3, 2, 1, 3];
-
-let freq1 = arrd.reduce((acc, cur) => {
-    acc[cur] = (acc[cur] || 0) + 1;
-    return acc
-}, {})
-console.log(freq1);
-
-
+const intersect3 = [...new Set(x1)].filter(item => setx2.has(item));
+console.log(intersect3)
