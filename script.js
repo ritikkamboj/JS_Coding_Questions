a74e91ef55c0b5ad80150fbcf872914975c3bfcb
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-10 17:28:16 +0530

    finding the second largest number from the array

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-// quetion to move all the zeroes at the end 
-// approach  : firstly we filter the non zero elemnts and then later we add the zeo elements in the last 
+// using set and sorting approach to find the second largest number 
 
+function secondLarge(arr) {
+    const newArr = [...new Set(arr)].sort((a, b) => b - a);
+    return newArr[1];
 
-function settle(arr) {
-
-    let arr1 = []
-    let nonzero = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== 0) {
-            arr1[nonzero] = arr[i];
-            nonzero++;
-        }
-    }
-
-    while (nonzero < arr.length) {
-        arr1[nonzero] = 0
-        nonzero++
-    }
-    return arr1
 }
 
-let z = settle([1, 0, 4, 0, 2, 3, 0]);
-console.log(z);
+console.log(secondLarge([10, 5, 20, 8]))
 
 
-// one more way of solution to solve the same problem 
-
-
-function settle1(arr) {
-    let nonzeroes = arr.filter((item) => item !== 0);
-    let zeroes = arr.filter((item) => item === 0)
-
-    return nonzeroes.concat(zeroes)
-}
-
-const newArr = settle1([1, 0, 4, 0, 2, 3, 0]);
-console.log(newArr)
+// second solution is yet to be understand and then we can able to solved it 
