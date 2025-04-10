f189200a55982d4d7bbb11654ea7b75b0d3a39a9
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-10 17:40:19 +0530

    checking palindromeic array

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// using set and sorting approach to find the second largest number 
+// Array to check that if its palindrome or not 
 
-function secondLarge(arr) {
-    const newArr = [...new Set(arr)].sort((a, b) => b - a);
-    return newArr[1];
+// one is minimal approach , you can say leyman approach by using while loop 
 
+function checkPalindrome(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] !== arr[right]) return false
+        left++;
+        right--;
+    }
+    return true
 }
 
-console.log(secondLarge([10, 5, 20, 8]))
+console.log(checkPalindrome([1, 2, 3, 2, 1]));
 
+// second approach is kind of modern approach 
 
-// second solution is yet to be understand and then we can able to solved it 
+function checkPalindrome1(arr) {
+    let reverse = [...arr].reverse();
+    console.log(reverse);
+    // have to change both arr in string so that we can compair them
+    return arr.join() === reverse.join();
+}
+
+console.log(checkPalindrome1([1, 2, 3, 2, 1]));
