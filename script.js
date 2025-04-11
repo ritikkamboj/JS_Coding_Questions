42d80049d2729cbec2cd9e9838c10db9d3dcc936
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-11 14:51:51 +0530

    ways to find the pair who gives a specific sum

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
-// we have to find the intersection of two arrays
+// Task is to find the pair of elements which results in the given sum 
+// 3 methods 
 
-const x1 = [1, 2, 3, 1];
-const x2 = [2, 3, 4, 1];
-
-const intersect = x1.filter((item) => x2.includes(item));
-console.log(intersect)
-
-// second solution is to use th set concept , because it makes resuls faster
-
-const setx2 = new Set(x2);
-const intersect2 = x1.filter((item) => setx2.has(item));
-console.log(intersect2)
+// 1.<ethod --> this method approach the elements from left to right 
 
 
-// 3rd silution is one liner solution 
+function findingPairs(arr, target) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target)
+                res.push([arr[i], arr[j]])
+        }
+    }
 
-const intersect3 = [...new Set(x1)].filter(item => setx2.has(item));
-console.log(intersect3)
+    return res;
+}
+
+console.log(findingPairs([1, 2, 3, 4, 5, 6], 6));
+
+// 2. In Second Approach, we consider the previous elements from the current elements
+
+
+function findingPairs1(arr, target) {
+    let res = [];
+    // let seen = new Set();
+    let arr1 = []
+
+    for (let item of arr) {
+        let complement = target - item;
+
+        // if (seen.has(complement))
+        //     res.push([complement, item])
+        if (arr1.includes(complement))
+            res.push([complement, item])
+
+
+
+        arr1.push(item)
+    }
+    return res;
+}
+
+console.log(findingPairs1([1, 2, 3, 4, 5, 6], 6))
+
+// 3. Approach we consider moving the pointer from the both directions as per requirement 
+
+function findingPairs2(arr, target) {
+    arr.sort((a, b) => b - a)
+    let left = 0;
+    let right = arr.length - 1;
+    let res = [];
+    while (left < right) {
+        if (arr[left] + arr[right] === target) {
+            res.push([arr[left], arr[right]])
+            left++;
+            right--;
+        }
+        else if (arr[left] + arr[right] < target)
+            left++;
+        else if (arr[left] + arr[right] > target)
+            right--;
+    }
+
+    return res;
+
+}
+console.log(findingPairs2([1, 2, 3, 4, 5, 6], 6))
+
+
+// let arr3 = [1, 2, 3, 4, 5, 6];
+// let arr5 = arr3.sort((a, b) => b - a);
+// console.log(arr5, arr3)
