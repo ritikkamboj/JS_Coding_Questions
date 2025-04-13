c441e7454498937f0b412bdeb778ac2d5a0a0d5d
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-13 12:52:29 +0530

    checking anagrams three ways sort() , map and frequency object

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-// program to capitalize the first letter of the each word in the sentence 
+// Program to check if two strings are anagrams or not 
+//  1. By using sort method 
 
-
-function capitalize(arr) {
-
-    let x = (arr.split(' ').map((item) => item[0].toUpperCase() + item.slice(1)).join(' '))
-    // console.log(arr)
-    console.log(x)
+function checkAnagrams(str1, str2) {
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
 
+// console.log('army', 'mary')
+console.log(checkAnagrams('army', 'mary'))
+
+// 2nd approach is bys using frequency approach(obj) and where obj approach comes we can apply Map approach too 
+
+function checkAnagrams1(str1, str2) {
+
+    if (str1.length !== str2.length) return;
+    let freq = {};
+
+    for (let item of str1) {
+        freq[item] = (freq[item] || 0) + 1
+    }
+
+    for (let item of str2) {
+        if (freq[item] === 0) return
+        freq[item]--;
+    }
+    return true
+}
+
+console.log(checkAnagrams('army', 'mary'))
 
 
-let arg = "hello cutie gpt"
-capitalize(arg)
+// 3rd way is using same approach above but wwith map function 
 
+function checkAnagrams2(str1, str2) {
+    if (str1.length !== str2.length) return false;
 
-// 2nd way by using regex way 
+    let map = new Map();
 
-function capitalize1(arr) {
-    let x = arr.replace('/\b\w/g', (char) => char.toUpperCase())
-    return x;
+    for (let item of str1) {
+        map.set(item, (map.get(item) || 0) + 1)
+    }
+    for (let item of str2) {
+        if (!map.get(item))
+            return false
+        map.set(item, map.get(item) - 1)
+    }
+
+    return true
+
 
 }
 
-capitalize(arg);
+console.log(checkAnagrams('army', 'mary'))
