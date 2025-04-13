f3fb1efadba4886496157aab258790dedd9f72c6
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-13 17:57:04 +0530

    removing duplicate element from the string

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-// Program to check if two strings are anagrams or not 
-//  1. By using sort method 
+// 1 . Removing all the duplicates characters from string 
+// 1/ first approach is by using set method 
 
-function checkAnagrams(str1, str2) {
-    return str1.split('').sort().join('') === str2.split('').sort().join('');
+function removeDuplicates(str) {
+
+    let seen = new Set();
+    result = ""
+
+    for (let item of str) {
+        if (!seen.has(item)) {
+            seen.add(item);
+            result += item;
+
+        }
+    }
+    return result;
 }
 
-// console.log('army', 'mary')
-console.log(checkAnagrams('army', 'mary'))
+console.log(removeDuplicates('aabbcccddd'))
 
-// 2nd approach is bys using frequency approach(obj) and where obj approach comes we can apply Map approach too 
+// 2. using built in methods like split(), indexOF and filter and join()
 
-function checkAnagrams1(str1, str2) {
-
-    if (str1.length !== str2.length) return;
-    let freq = {};
-
-    for (let item of str1) {
-        freq[item] = (freq[item] || 0) + 1
-    }
-
-    for (let item of str2) {
-        if (freq[item] === 0) return
-        freq[item]--;
-    }
-    return true
+function removeDuplicates1(str) {
+    return str.split('').filter((num, index, arr) => arr.indexOf(num) === index).join('');
 }
 
-console.log(checkAnagrams('army', 'mary'))
+console.log(removeDuplicates('aabbcccddd'))
+// console.log(removeDuplicates1(''))
 
+// 3rd way using map Method 
 
-// 3rd way is using same approach above but wwith map function 
-
-function checkAnagrams2(str1, str2) {
-    if (str1.length !== str2.length) return false;
-
+function removeDuplicates2(str) {
     let map = new Map();
+    let result = ''
 
-    for (let item of str1) {
-        map.set(item, (map.get(item) || 0) + 1)
+    for (let item of str) {
+        if (!map.has(item)) {
+            map.set(item, true)
+            result += item;
+        }
     }
-    for (let item of str2) {
-        if (!map.get(item))
-            return false
-        map.set(item, map.get(item) - 1)
-    }
+    return result;
 
-    return true
 
 
 }
 
-console.log(checkAnagrams('army', 'mary'))
+console.log(removeDuplicates2('aabbcccddd'))
