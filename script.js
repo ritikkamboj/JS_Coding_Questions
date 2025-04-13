68a43afe057c6ea51102b76849e80dc4ab9cdcab
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-13 23:23:20 +0530

    finding the most frequent character in the string

--- script.js
+++ script.js
@@ -1,44 +1,89 @@
-// checking if two strings are rotation of each other 
+// Finding the most frequent character in the string 
 
-// 1. Ist approach is combining the str1 2 times and then checking str2 
+// Ist Method is using object to count the frequency 
 
-function strChecking(str1, str2) {
-    let combined = str1 + str1;
-    if (combined.includes(str2))
-        return true;
-    return false
-}
+function countFreq(str) {
+    let freq = {};
+    let maxCount = 0;
+    let maxChar = '';
 
-console.log(strChecking('ABCD', "CDAB")
-)
 
-// 2nd method is manial ROation and checking 
+    for (let item of str) {
+        freq[item] = (freq[item] || 0) + 1;
 
-function strChecking1(str1, str2) {
-    // let str = str1.split('');
-    for (let i = 0; i < str1.length; i++) {
-        let combine = str1.slice(i) + str1.slice(0, i);
-        if (combine === str2)
-            return true
+        if (freq[item] > maxCount) {
+            maxCount = freq[item]
+
+            maxChar = item;
+
+        }
     }
-    return falsex
+
+    return maxChar;
+
+
+
 }
-console.log(strChecking1('ABCD', "CDAB"));
+
+console.log(countFreq('aabbbccccdd'));
 
 
-// using Array way and psuh method 
+
+// we are using map method and mostly approach remain same as above method 
 
 
-function strChecking2(str1, str2) {
-    let str = str1.split('');
 
-    for (let i = 0; i < str1.length; i++) {
-        str.push(str.shift());
-        if (str.join('') === str2)
-            return true
+
+function countFreq1(str) {
+    let map = new Map();
+    let maxCount = 0;
+    let maxChar = '';
+
+    for (let item of str) {
+        map.set(item, (map.get(item) || 0) + 1)
+
+        if (map.get(item) > maxCount) {
+            maxCount = map.get(item);
+            maxChar = item
+        }
     }
-    return false;
+    return maxChar;
+}
+
+console.log(countFreq1('aabbbccccdd'));
+
+
+// 3rd method using split() and reduce method 
+
+const userProfile = {
+    id: 101,
+    name: "CutiePie",
+    age: 24,
+    isOnline: true,
+    hobbies: ["coding", "coffee", "memes"],
+    address: {
+        city: "Codeville",
+        zip: "12345",
+        country: "JS Land"
+    },
+}
+
+console.log(Object.keys(userProfile))
+
+
+// 3rd way is using reduce method ( we have to use two times reduce method )
+
+function countFreq2(str) {
+    let freq = str.split('').reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+
+    }, {})
+
+    console.log(freq)
+
+    return Object.keys(freq).reduce((a, b) => freq[a] > freq[b] ? a : b);
 
 }
 
-console.log(strChecking2('ABCD', "CDAB"));
+console.log(countFreq2('aabbbccccdd'));
