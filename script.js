2fe9061cfbaed0de2b3e62d90093d3cc6fc1c499
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-14 18:30:38 +0530

    finding the highest length word in the sentence

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-//  Compress a string means Run length encoding
+// Program to find out the longest word in the sentence 
 
-// 1. simple array method 
+// 1. Started with Basic Loop 
 
-function compressString(str) {
+function longestWord(str) {
+    let longest = '';
+    let words = str.split(' ');
 
-    let result = "";
-    let count = 1;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === str[i + 1])
-            count++;
-        else {
-            result = result + str[i] + count;
-            count = 1;
-        }
+    for (let word of words) {
+        if (word.length > longest.length)
+            longest = word
+
+
     }
-
-    return result;
+    return longest;
 }
 
-console.log(compressString('aabbccddd'))
+console.log(longestWord('I am a Master Web Developer'))
 
+// 2nd way is to use reduce method 
 
-// 2.using method array , but inside working is same as above approach 
-
-function compressString1(str) {
-    let result = [];
-    let count = 1;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === str[i + 1])
-            count++;
-        else {
-            result.push(str[i] + count);
-            count = 1;
-        }
-    }
-
-    return result.join('');
+function longestWord1(str) {
+    return str.split(' ').reduce((acc, cur) => cur.length > acc.length ? cur : acc);
 }
 
-console.log(compressString1('aabbccddd'))
+console.log(longestWord1('I am a Master Web Developer'))
+
+// 3. way is by sorting the array 
+
+function longestWord2(str) {
+    return str.split(' ').sort((a, b) => b.length - a.length)[0];
+}
+
+console.log(longestWord2('I am a Master Web Developer'))
