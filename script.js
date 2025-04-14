64b9598342093dd40f17139384a8032e34b02675
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-14 18:51:28 +0530

    changing from Kabab case to camel Case

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// counting the word in the sentence 
+//changing the sentence from Kebab case to Camel case
 
-function counting(str) {
-    return str.trim().split(' ').length;
+// 1. method  by using map and split()
+
+function camelCase(str) {
+    return str.split('-').map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1)).join('')
 }
 
-console.log(counting(" I will Become the greatest Web Developer "))
+console.log(camelCase("hello-sweetie-world"))
+
+
+// 2. By using reduce method 
+
+
+function camelCase1(str) {
+    return str.split('-').reduce((camel, word, index) => {
+        if (index === 0) return word; else {
+            return camel + word[0].toUpperCase() + word.slice(1)
+        }
+    }, "")
+}
+
+console.log(camelCase1("hello-sweetie-world"))
