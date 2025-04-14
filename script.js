37b399dfa1bda56587b156cd0fbdb0a24b165923
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-14 18:40:01 +0530

    counting the number of words in the sentence

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-// Program to find out the longest word in the sentence 
+// counting the word in the sentence 
 
-// 1. Started with Basic Loop 
-
-function longestWord(str) {
-    let longest = '';
-    let words = str.split(' ');
-
-    for (let word of words) {
-        if (word.length > longest.length)
-            longest = word
-
-
-    }
-    return longest;
+function counting(str) {
+    return str.trim().split(' ').length;
 }
 
-console.log(longestWord('I am a Master Web Developer'))
-
-// 2nd way is to use reduce method 
-
-function longestWord1(str) {
-    return str.split(' ').reduce((acc, cur) => cur.length > acc.length ? cur : acc);
-}
-
-console.log(longestWord1('I am a Master Web Developer'))
-
-// 3. way is by sorting the array 
-
-function longestWord2(str) {
-    return str.split(' ').sort((a, b) => b.length - a.length)[0];
-}
-
-console.log(longestWord2('I am a Master Web Developer'))
+console.log(counting(" I will Become the greatest Web Developer "))
