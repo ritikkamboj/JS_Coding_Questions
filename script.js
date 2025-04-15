3a78c8ac9c3cb813b78cf833b4504b602fa4e281
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-15 17:29:02 +0530

    solutions to if two strings are one edit away or not

--- script.js
+++ script.js
@@ -1,23 +1,135 @@
-//changing the sentence from Kebab case to Camel case
+// Question to check if two strings are one edit away from each other 
 
-// 1. method  by using map and split()
+function compare(str1, str2) {
+    let len1 = str1.length;
+    let len2 = str2.length;
 
-function camelCase(str) {
-    return str.split('-').map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1)).join('')
-}
+    // Now if the string length difference gets more than 1 
 
-console.log(camelCase("hello-sweetie-world"))
+    if (Math.abs(len1 - len2) > 1) return false;
 
+    // Now we have to find out which string is greater and which is shorter 
 
-// 2. By using reduce method 
+    let [longer, shorter] = len1 > len2 ? [str1, str2] : [str2, str1];
 
+    let i = 0, j = 0, foundDiff = false
 
-function camelCase1(str) {
-    return str.split('-').reduce((camel, word, index) => {
-        if (index === 0) return word; else {
-            return camel + word[0].toUpperCase() + word.slice(1)
+    while (i < shorter.length && j < longer.length) {
+        if (shorter[i] !== longer[j]) {
+            if (foundDiff) return false;
+            foundDiff = true;
+
+            if (len1 === len2)
+                i++
+
         }
-    }, "")
+        else {
+            i++;
+        }
+        j++;
+
+    }
+
+    return true;
 }
 
-console.log(camelCase1("hello-sweetie-world"))
+// console.log(compare("pale", "ple"));  // true
+console.log(compare("pale", "ple"));  // true
+console.log(compare("pale", "bale")); // true
+console.log(compare("pale", "bake")); // false
+
+
+// 2n solution is feels more easier as compared to ist one 
+
+function compare1(str1, str2) {
+    if (Math.abs(str1.length - str2.length) > 1) return false;
+
+    let i = 0, j = 0, diff = 0;
+
+    while (i < str1.length && j < str2.length) {
+        if (str1[i] !== str2[j]) {
+            if (++diff > 1) return false;
+            if (str1.length > str2.length)
+                i++;
+            else if (str2.length > str1.length)
+                j++;
+            else {
+                i++;
+                j++;
+            }
+
+        }
+        else {
+            i++;
+            j++;
+        }
+    }
+    return true;
+}
+
+
+
+// console.log(compare("pale", "ple"));  // true
+console.log(compare1("pale", "ple"));  // true
+console.log(compare1("pale", "bale")); // true
+console.log(compare1("pale", "bake")); // false
+
+
+
+// 3rd way is to make separate functions for the calculations 
+
+
+function compare2(str1, str2) {
+    if (str1.length === str2.length)
+        return oneReplace(str1, str2)
+    else if (str1.length + 1 === str2.length) {
+        return oneInsert(str1, str2)
+
+    }
+    else if (str1.length - 1 === str2.length) {
+        return oneInsert(str2, str1)
+    }
+    else {
+        return false;
+    }
+}
+
+
+function oneReplace(str1, str2) {
+    let foundDiff = false;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] !== str2[i]) {
+            if (foundDiff) return false;
+            foundDiff = true;
+        }
+    }
+    return true;
+}
+
+function oneInsert(str1, str2) {
+    let i = 0, j = 0, foundDiff = false;
+
+    while (i < str1.length && j < str2.length) {
+        if (str1[i] !== str2[j]) {
+            if (foundDiff) return false;
+            foundDiff = true;
+
+            if (str1.length === str2.length)
+                i++;
+        }
+        else {
+            i++
+        }
+
+        j++;
+    }
+
+    return true;
+
+}
+
+
+// console.log(compare("pale", "ple"));  // true
+console.log(compare2("pale", "ple"));  // true
+console.log(compare2("pale", "bale")); // true
+console.log(compare2("pale", "bake")); // false
